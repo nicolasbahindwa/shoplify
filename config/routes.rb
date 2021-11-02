7720451b137f7f7941c8c69e7a579fe629995bee
nicolasbahindwa@gmail.com <nicolasbahindwa@gmail.com>
2021-11-02 10:43:32 +0900

    session based shoping cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "success", to: "checkout#success"
   get "failure", to: "checkout#failure"
   get "cancel", to: "checkout#cancel"
+  post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
+  delete "products/remove_from_cart/:id", to: "products#remove_from_cart", as: "remove_from_cart"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
